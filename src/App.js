a5819bab771fb07cb3a0262e99875e8899637323
miguelsandovaldev <miguelsandovaldev@gmail.com>
2020-09-15 10:15:25 -0400

    Added dropdown component with body event listener handling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 // import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+// import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 // const items = [
 //   {
@@ -17,11 +18,37 @@ import Search from "./components/Search";
 //   },
 // ];
 
+const options = [
+  {
+    label: "Mountain",
+    value: "red",
+  },
+  {
+    label: "Forest",
+    value: "green",
+  },
+  {
+    label: "Island",
+    value: "blue",
+  },
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
 };
